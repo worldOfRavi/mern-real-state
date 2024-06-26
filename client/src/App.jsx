f75431008e3fb048f0e0a0b3d24fab716ab0bb23
worldOfRavi <rajendra20023@gmail.com>
2024-06-25 20:48:14 -0400

    created header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import About from './pages/About';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <>
-  <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path='/profile' element={<Profile />} />
-    <Route path='/about' element={<About />} />
-    <Route path='/signin' element={<SignIn />} />
-    <Route path='/signup' element={<SignUp />} />
-  </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
     </>
-  )
+  );
 }
