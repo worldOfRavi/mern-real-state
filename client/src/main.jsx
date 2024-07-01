dccb7c6ce19f9db2b55b250149e3743b5b6ed6b8
worldOfRavi <rajendra20023@gmail.com>
2024-07-01 16:00:03 -0400

    added redux persist to store info in localstorage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,16 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux'
-import { store } from './redux/store.js';
+import { persistor, store } from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <BrowserRouter>
   <Provider store={store}>
+  <PersistGate loading={null} persistor={persistor}>
     <App />
+  </PersistGate>
   </Provider>
   </BrowserRouter>
   </React.StrictMode>,
