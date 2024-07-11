55ea2a9882fde73b8344bedc60f96dc381e10ae1
worldOfRavi <rajendra20023@gmail.com>
2024-07-11 18:48:46 -0400

    image upload functionalityin listing completed

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,63 @@
-import React from "react";
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import React, { useState } from "react";
+import { app } from "../firebase";
 
 const CreateListing = () => {
+    const [files, setFiles] = useState([]);
+    const [formData, setFormData] = useState({
+        imageUrl:[]
+    })
+    const [imageUploadError, setImageUploadError] = useState(false);
+    const [uploading, setUploading] = useState(false);
+    console.log(formData);
+    const handleImageUpload = ()=>{
+        if(files.length > 0 && files.length + formData.imageUrl.length < 7){
+            setUploading(true);
+            const promises = [];
+            for(let i = 0;i<files.length;i++){
+                promises.push(storeImage(files[i]))
+            }
+            Promise.all(promises).then((urls)=>{
+                setFormData({...formData,imageUrl:formData.imageUrl.concat(urls)})
+                setImageUploadError(false)
+                setUploading(false)
+            }).catch((err)=>{
+                setImageUploadError("Image upload failed (2 mb max per image)");
+                setUploading(false)
+            })
+        }else{
+            setImageUploadError('You can uplaod only 6 images per listing');
+        }
+    };
+    const storeImage = async(file)=>{
+        return new Promise((resolve, reject)=>{
+            const storage = getStorage(app);
+            const fileName = new Date().getTime() + file.name;
+            const storageRef = ref(storage, fileName);
+            
+            const uploadTask = uploadBytesResumable(storageRef, file);
+            uploadTask.on("state_changed",
+                (snapshot)=>{
+                    const progress = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
+                    console.log(`Upload is ${progress}% done`);
+                },
+                (error)=>{
+                    reject(error);
+                },
+                ()=>{
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl)=>{
+                        resolve(downloadUrl);
+                    })
+                }
+
+            )
+        })
+    }
+    const handleImageDelete = (id) =>{
+        setFormData({
+            ...formData, imageUrl:formData.imageUrl.filter((_,index)=>index!=id)
+        })
+    }
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="font-semibold text-3xl text-center my-7">
@@ -34,29 +91,29 @@ const CreateListing = () => {
             required
           />
 
-          <div className="flex gap-6 flex-wrap">
+          <div className="flex gap-4 flex-wrap">
             <div className="flex gap-2">
-              <input type="checkbox" id="sale" className="w-5" />
+              <input type="checkbox" id="sale" className="w-4" />
               <span>Sell</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="rent" className="w-5" />
+              <input type="checkbox" id="rent" className="w-4" />
               <span>Rent</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="parking" className="w-5" />
+              <input type="checkbox" id="parking" className="w-4" />
               <span>Parking Spot</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="furnished" className="w-5" />
+              <input type="checkbox" id="furnished" className="w-4" />
               <span>Furnished</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="offer" className="w-5" />
+              <input type="checkbox" id="offer" className="w-4" />
               <span>Offer</span>
             </div>
           </div>
-          <div className="flex gap-6 flex-wrap">
+          <div className="flex gap-4 flex-wrap">
             <div className="flex gap-2 items-center ">
               <input
                 type="number"
@@ -123,14 +180,24 @@ const CreateListing = () => {
               multiple
               accept="image/*"
               className="p-3 border border-gary-300 rounded w-full"
+              onChange={(e)=>setFiles(e.target.files)}
             />
-            <button
+            <button type="button" onClick={handleImageUpload} disabled={uploading}
               className="border border-green-700 text-green-700 uppercase p-3
              rounded hover:shadow-lg disabled:opacity-85"
             >
-              upload
+              {uploading ? "uploading" : "upload"}
             </button>
           </div>
+            <p className="text-red-700 text-xs">{imageUploadError && imageUploadError}</p>
+            {
+            formData.imageUrl.map((url,index)=>(
+                <div key={url} className="flex justify-between items-center border  p-2">
+                    <img src={url} className="w-20 h-20 object-contain rounded-lg" />
+                    <button type="button" className="text-red-700 hover:opacity-75" onClick={()=>handleImageDelete(index)}>Delete</button>
+                </div>
+            ))
+            }
           <button className="uppercase p-3 border bg-slate-700 text-white rounded-lg hover:opacity-90 disabled:opacity-80 ">create listing</button>
         </div>
       </form>
