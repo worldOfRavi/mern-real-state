66df67cc846112099ad61c726a315a7398aed307
worldOfRavi <rajendra20023@gmail.com>
2024-07-15 16:17:24 -0400

    added search api

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createListing, deleteListing, getListing, updateListing } from "../controllers/listing.controller.js";
+import { createListing, deleteListing, getListing, updateListing,getListings } from "../controllers/listing.controller.js";
 const router = express.Router();
 import {verifyUser} from "../utils/verifyUser.js"
 
@@ -7,4 +7,5 @@ router.route("/create").post( verifyUser,createListing);
 router.route("/delete/:id").delete( verifyUser,deleteListing);
 router.route("/update/:id").post(verifyUser, updateListing)
 router.route("/get/:id").get(getListing)
+router.route("/get").get(getListings)
 export default router;
