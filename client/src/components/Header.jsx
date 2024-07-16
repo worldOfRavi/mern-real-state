1312bcf27ba1761ff5f324a18ed7e7255cc63e4d
worldOfRavi <rajendra20023@gmail.com>
2024-07-16 17:26:59 -0400

    added onChange and on submit functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 export default function Header() {
+  const navigate = useNavigate();
   const {currentUser} = useSelector((state)=>state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    const urlPrams = new URLSearchParams(window.location.search);
+    urlPrams.set("searchTerm",searchTerm);
+    const searchQuery = urlPrams.toString();
+    navigate(`/search/?${searchQuery}`);
+  }
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -14,13 +31,17 @@ export default function Header() {
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
-        <form className="flex items-center bg-slate-100 rounded-lg p-3 w-24 sm:w-64">
+        <form onSubmit={handleSubmit} className="flex items-center bg-slate-100 rounded-lg p-3 w-24 sm:w-64">
           <input
             type="text"
             placeholder="Search..." 
             className="bg-transparent focus:outline-none"
+            value={searchTerm}
+            onChange={(e)=>setSearchTerm(e.target.value)}
           />
+          <button>
           <FaSearch className="text-slate-600" />
+          </button>
         </form>
         <ul className="flex gap-4">
           <li className="hidden sm:inline transition-all duration-300 delay-100 hover:underline">
