4858fe58642abb26a754a3e2310ecfd5fc37131a
worldOfRavi <rajendra20023@gmail.com>
2024-11-15 13:22:25 -0500

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,10 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from 'path';
+
+const __dirname = path.resolve();
+// __dirname contains the current running directory
 const app = express();
 
 // const PORT = process.env.PORT || 3000;
@@ -15,6 +19,13 @@ app.use(cookieParser());
 app.use("/api/auth",authRouter);
 app.use("/api/user",userRouter);
 app.use("/api/listing",listingRouter);
+
+// when we run the build command, a dist folder is created containing index.html file.
+app.use(express.static(path.join(__dirname, "/client/dist")));
+// when we run the site and the url is other than then above then then following command get executed where it send the static file index.htmll located in dist folder
+app.get("*",(req, res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'));
+})
 
 
 app.use((err, req, res, next)=>{
